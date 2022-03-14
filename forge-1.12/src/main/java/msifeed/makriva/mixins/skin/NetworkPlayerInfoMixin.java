d8243507e0fa51f534c0dddb5d36c32c34e816b3
msifeed <remsifeed@gmail.com>
2022-03-14 23:33:20 +0200

    Fix slim model type from metadata

--- forge-1.12/src/main/java/msifeed/makriva/mixins/skin/NetworkPlayerInfoMixin.java
+++ forge-1.12/src/main/java/msifeed/makriva/mixins/skin/NetworkPlayerInfoMixin.java
@@ -2,6 +2,7 @@ package msifeed.makriva.mixins.skin;
 
 import com.mojang.authlib.GameProfile;
 import msifeed.makriva.Makriva;
+import msifeed.makriva.model.Shape;
 import net.minecraft.client.network.NetworkPlayerInfo;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NetworkPlayerInfo.class)
 public class NetworkPlayerInfoMixin {
@@ -22,5 +24,12 @@ public class NetworkPlayerInfoMixin {
         if (!Makriva.MODELS.hasShape(gameProfile.getId())) {
             ci.cancel();
         }
+    }
+
+    @Inject(method = "getSkinType", at = @At("HEAD"), cancellable = true)
+    public void getSkinType(CallbackInfoReturnable<String> cir) {
+        final Shape shape = Makriva.MODELS.getShape(gameProfile.getId());
+        final String modelType = shape.metadata.get("model");
+        if (modelType != null) cir.setReturnValue(modelType);
     }
 }
